84ccd3a57626588f4e2b1fa852b622fda8b33383
florianm <florian.mayer@dpaw.wa.gov.au>
2017-08-15 15:06:27 +0800

    add enzyme tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import 'jest-enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders without crashing', () => {
+  shallow(<App />);
+});
+
+it('renders welcome message', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h1>Turtles WA</h1>;
+  //expect(wrapper.contains(welcome)).toEqual(true);
+  expect(wrapper).toContainReact(welcome); // jest-enzyme
 });
