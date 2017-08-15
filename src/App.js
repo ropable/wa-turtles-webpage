e52d1a303924073393e7bef7b37a7950c294b986
florianm <florian.mayer@dpaw.wa.gov.au>
2017-08-15 14:24:29 +0800

    change title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import logo from './logo.svg';
 import './App.css';
+
+injectTapEventPlugin();
 
 class App extends Component {
   render() {
@@ -8,10 +13,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Turtles WA</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Welcome to the new web page for Turtles WA. Test!
         </p>
       </div>
     );
