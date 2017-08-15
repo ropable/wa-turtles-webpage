9ee5af2a9faa138c681257ec72ad0a47859992e7
florianm <florian.mayer@dpaw.wa.gov.au>
2017-08-15 14:45:30 +0800

    add dummy content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 // @flow
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+import {
+  Grid,
+  Navbar,
+  Nav,
+  NavItem,
+  NavDropdown,
+  MenuItem,
+  Jumbotron,
+  Button
+} from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,15 +18,42 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Navbar inverse fixedTop>
-          <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">Turtles WA</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-          </Grid>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Turtles WA</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="#">
+                Link
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link
+              </NavItem>
+              <NavDropdown
+                eventKey={3}
+                title="Dropdown"
+                id="basic-nav-dropdown"
+              >
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.3}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">
+                Link Right
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link Right
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <Jumbotron>
           <Grid>
