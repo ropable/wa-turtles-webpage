d452eaebb2465934c87b088c5f4c066dcbee51da
florianm <florian.mayer@dpaw.wa.gov.au>
2017-08-16 11:06:33 +0800

    first light CKAN datasets from CKAN API

--- src/App.js
+++ src/App.js
@@ -15,7 +15,72 @@ import {
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      datasets: null,
+      infoStatus: 'loading'
+    };
+  }
+  _get_datasets = datasets => {
+    const main = this;
+    main.setState({ infoStatus: 'loading' });
+    var ckanApiUrl = 'https://data.dpaw.wa.gov.au/api/3/action/';
+    var url = ckanApiUrl + 'package_search?q=tags:public';
+
+    fetch(url)
+      .then(function(response) {
+        return response;
+      })
+      .then(function(response) {
+        setTimeout(function() {
+          main.setState({
+            infoStatus: 'loaded'
+          });
+        }, 300);
+        return response.json();
+      })
+      .then(function(data) {
+        main.setState({ datasets: data.result.results });
+      })
+      .catch(function() {
+        main.setState({ infoStatus: 'error' });
+      });
+  };
+
+  // componentWillMount() {
+  //     this._get_datasets();
+  // };
+
+  componentDidMount() {
+    this._get_datasets();
+  }
+
   render() {
+    const { datasets, infoStatus } = this.state;
+    let data = null;
+
+    if (infoStatus === 'loaded') {
+      data = (
+        <ul>
+          {datasets.map(function(ds) {
+            return (
+              <li>
+                {ds.title} {ds.id}
+              </li>
+            );
+          })}
+        </ul>
+      );
+    } else if (infoStatus === 'loading') {
+      data = <div className="info loading">Loading public datasets...</div>;
+    } else if (infoStatus === 'error') {
+      data = (
+        <div className="info error">
+          Error while loading public datasets. Try again later.
+        </div>
+      );
+    }
     return (
       <div>
         <Navbar inverse collapseOnSelect>
@@ -68,6 +133,7 @@ class App extends Component {
                 A button!
               </Button>
             </p>
+            {data}
           </Grid>
         </Jumbotron>
       </div>
