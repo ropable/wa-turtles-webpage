e7df09d247dde8927774db75b9c784cd6b62db3c
florianm <florian.mayer@dpaw.wa.gov.au>
2017-08-21 13:21:14 +0800

    style projects

--- src/ProjectRow.js
+++ src/ProjectRow.js
@@ -8,24 +8,24 @@ class ProjectRow extends Component {
     const pro = this.props.project;
     const sdisUrl = 'https://sdis.dpaw.wa.gov.au';
     let imgsrc = pro.image ? sdisUrl + '/media/' + pro.image : logo;
+    function markDangerous(cantBelieveItsNotHtml) {
+      return { __html: cantBelieveItsNotHtml };
+    }
 
     return (
       <Col xs={12} md={6} lg={4}>
         <Thumbnail src={imgsrc} alt={pro.project_type_year_number_plain}>
           <h4>{pro.title_plain}</h4>
           <p>
-            <Glyphicon glyph="qrcode" />&nbsp;{pro.project_type_year_number_plain}
+            <Glyphicon glyph="wrench" /> {pro.project_type_year_number_plain}{' '}
+            {pro.status_display}
             <br />
-            <Glyphicon glyph="wrench" />&nbsp;{pro.status_display}
+            <Glyphicon glyph="user" /> {pro.team_list_plain}
             <br />
-            <Glyphicon glyph="comment" />&nbsp;{pro.tagline_plain}
-            <br />
-            <Glyphicon glyph="user" />&nbsp;{pro.team_list_plain}
-            <br />
-            <Glyphicon glyph="home" />&nbsp;{pro.program}
+            <Glyphicon glyph="home" /> {pro.program}
           </p>
-          <Panel collapsible header="Details">
-            <span dangerouslySetInnerHTML={pro.comments} />
+          <Panel collapsible header={pro.tagline_plain}>
+            <span dangerouslySetInnerHTML={markDangerous(pro.comments)} />
           </Panel>
           <Button
             bsStyle="primary"
