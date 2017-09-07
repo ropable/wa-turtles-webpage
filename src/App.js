a5ed999e32aae55a7212115721f90dbd85b42903
florianm <florian.mayer@dpaw.wa.gov.au>
2017-09-07 12:31:06 +0800

    add test to App.test.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 // @flow
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 // import { applyMiddleware, createStore, compose } from "redux";
 // import { offline } from "redux-offline";
 // import offlineConfig from "redux-offline/lib/defaults";
 
-import './App.css';
-import Navigation from './Navigation/Navigation';
-import Dashboard from './Dashboard/Dashboard';
-import Education from './Education/Education';
-import Datasets from './Datasets/Datasets';
-import Projects from './Projects/Projects';
-import Locations from './Locations/Locations';
-import Footer from './Footer/Footer';
+import "./App.css";
+import Navigation from "./Navigation/Navigation";
+import Dashboard from "./Dashboard/Dashboard";
+import Education from "./Education/Education";
+import Datasets from "./Datasets/Datasets";
+import Projects from "./Projects/Projects";
+import Locations from "./Locations/Locations";
+import Footer from "./Footer/Footer";
 
 export default class App extends React.Component<{}> {
   render() {
@@ -21,7 +21,7 @@ export default class App extends React.Component<{}> {
         <div>
           <Navigation />
           <Route exact path="/" component={Dashboard} />
-          <Route exact path={'/education'} component={Education} />
+          <Route exact path={"/education"} component={Education} />
           <Route exact path="/datasets" component={Datasets} />
           <Route
             exact
--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { shallow } from 'enzyme';
-import 'jest-enzyme';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import { shallow } from "enzyme";
+import "jest-enzyme";
 
-import App from './App';
-import Dashboard from './Dashboard/Dashboard';
+import App from "./App";
+import Dashboard from "./Dashboard/Dashboard";
+import Datasets from "./Datasets/Datasets";
+import Navigation from "./Navigation/Navigation";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
 });
 
-it('renders shallow without crashing', () => {
+it("renders shallow without crashing", () => {
   shallow(<App />);
+});
+
+it("renders Navigation component", () => {
+  const wrapper = shallow(<App />);
+  const msg = <Navigation />;
+  expect(wrapper).toContainReact(msg);
 });
